77dd8c7d079eef9860a30710ed0321fdee112a07
DaeWook, Kim <kimdaewook@witstudio.net>
2015-12-27 18:05:47 +0900

    remve unused variable
    performance variable is does not supported ios browser.
    and performance variable is never used.
    
    so i just remove performance.now line.

--- src/parser.js
+++ src/parser.js
@@ -14,7 +14,7 @@ var PNG_SIGNATURE_BYTES = new Uint8Array([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x
 module.exports = function (buffer) {
     var bytes = new Uint8Array(buffer);
     return new Promise(function (resolve, reject) {
-        var t0 = performance.now();
+        
         for (var i = 0; i < PNG_SIGNATURE_BYTES.length; i++) {
             if (PNG_SIGNATURE_BYTES[i] != bytes[i]) {
                 reject("Not a PNG file (invalid file signature)");
