60d7b4f95543219091fe8305a63c6cb3d5feedf8
lizmlzhou <lizmlzhou@tencent.com>
2020-01-04 13:59:10 +0800

    fix: use loader in a async way

--- src/loader.js
+++ src/loader.js
@@ -5,7 +5,7 @@ var Promise = Promise || require('es6-promise').Promise;
 module.exports = function (url) {
     return new Promise(function (resolve, reject) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
+        xhr.open('GET', url, true);
         xhr.responseType = 'arraybuffer';
         xhr.onload = function () {
             if (this.status == 200) {
